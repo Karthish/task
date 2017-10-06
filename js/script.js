03a1a8cb11ba3da0215bd4a95c26bb41b69527b0
Manikandan Moorthi <manikandan.moorthi@mantralabsglobal.com>
2017-10-06 07:15:55 +0530

    task changes added

--- js/script.js
+++ js/script.js
@@ -1,32 +1,27 @@
 $( document ).ready(function() {
-	// sidebar expand/collapsed
-	$('.btn-expand-collapse').click(function(e) {
-		$('.navbar-primary').toggleClass('collapsed');
-		$(this).toggleClass('collapsedBtn');
-		if($(this).hasClass('collapsedBtn')) {
-			$(this).children('span').text('EXPAND');
-			$(this).find('.fa').toggleClass('fa-angle-left fa-angle-right');
-		} else {
-			$(this).children('span').text('COLLAPSE');
-			$(this).find('.fa').toggleClass('fa-angle-right fa-angle-left');
-		}
-	});
+    // sidebar expand/collapsed
+    $('.btn-expand-collapse').click(function() {
+        $('.navbar-primary').toggleClass('collapsed');
+        $(this).toggleClass('collapsedBtn');
+        $(this).children('.fa').toggleClass('fa-angle-right');
+    });
 
-	// On hover-actions
-	$('.menu-option').mouseover(function() {
-		if($(this).children().hasClass('sub-menu')) {
-			// $(this).children('.sub-menu').show();
-			if($(this).closest('.navbar-primary').hasClass('collapsed')) {
-				$('.navbar-primary-menu-footer').show();
-				$('.submenu-expand-footer').hide();
-			} else {
-				$('.navbar-primary-menu-footer').hide();
-				$('.submenu-expand-footer').show();
-			}
-		}
-		}).mouseout(function() {
-    	// $(this).children('.sub-menu').hide();
-    	$('.navbar-primary-menu-footer').show();
-		$('.submenu-expand-footer').hide();
-	});
+
+
+    // On hover check whether it is collapsed panel or expanded panel
+    // in expanded panel, when submenu is visible change the footer style(only show icons)
+    $('.menu-option').mouseover(function() {
+        if($(this).children('ul').hasClass('sub-menu')) {
+            if($(this).closest('.navbar-primary').hasClass('collapsed')) {
+                $('.navbar-primary-menu-footer').show();
+                $('.submenu-expand-footer').hide();
+            } else {
+                $('.navbar-primary-menu-footer').hide();
+                $('.submenu-expand-footer').show();
+            }
+        }
+        }).mouseout(function() {
+        $('.navbar-primary-menu-footer').show();
+        $('.submenu-expand-footer').hide();
+    });
 });
